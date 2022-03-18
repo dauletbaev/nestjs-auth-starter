801d14d653bc2948de4b5cb286cd328e88de86fd
Abat Dauletbaev <abat.dauletbayev@gmail.com>
2022-03-19 00:57:14 +0500

    fastify version

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: false }), // disable logger
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
-  await app.listen(3000);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
